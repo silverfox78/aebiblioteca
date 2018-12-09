21e94f382fdce2bdae9ee90e4d5cf216ad6657f5
Samuel Barrera <sbarrera@gmail.com>
2018-12-09 20:22:59 -0300

    Actualizacion

--- src/main/java/cl/ciisa/services/SrvLibro.java
+++ src/main/java/cl/ciisa/services/SrvLibro.java
@@ -13,6 +13,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import cl.ciisa.data.DBLibro;
+import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 
 @Path("/libros")
 public class SrvLibro {
@@ -33,6 +35,37 @@ public class SrvLibro {
             }
         } catch (Exception e) {
             return Response.serverError().entity("Error al listar los libros - " + e.getMessage()).build();
+        }
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response guardarLibro(DBLibro libro){
+        try {            
+            em = emf.createEntityManager();
+            //em.getTransaction().begin();
+            em.persist(libro);
+            //em.getTransaction().commit();
+            
+            return Response.status(Response.Status.CREATED).build();
+        } catch (Exception e) {
+            return Response.serverError().entity("Error al guardar el libro - " + e.getMessage()).build();
+        }
+    }
+    
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response actualizaLibro(DBLibro libro){
+        try {
+            em = emf.createEntityManager();
+            em.persist(libro);
+            em.getTransaction().begin();
+            libro = em.merge(libro);
+            em.getTransaction().commit();            
+            
+            return Response.status(Response.Status.OK).build();
+        } catch (Exception e) {
+            return Response.serverError().entity("Error al actualizar el libro - " + e.getMessage()).build();
         }
     }
 }
